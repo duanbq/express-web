d19a03072b54ec3dd996e0ce7caf69d0ad67dbe4
duanbq <283775162@qq.com>
2017-02-22 15:15:19 +0800

    使用文件模块

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+var fortune = require('./lib/fortune.js');
+
 var app = express();
 
 // set up handlebars view engine
@@ -7,26 +9,19 @@ var handlebars = require('express3-handlebars')
 	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('view','./view');
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-var fortuneCookies = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
+
 
 app.get('/', function(req, res) {
 	res.render('home');
 });
 app.get('/about', function(req,res){
-	var randomFortune =
-		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-	res.render('about', { fortune: randomFortune });
+
+	res.render('about', { fortune: fortune.getFortune() });
 });
 
 // 404 catch-all handler (middleware)
